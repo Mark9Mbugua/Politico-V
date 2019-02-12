1d9f874531efb50b023dfd52833cab101b223c86
Mark Mbugua <mbuguamark@gmail.com>
2019-02-13 02:19:28 +0300

    [Starts #163914022] configure postgres

--- instance/config.py
+++ instance/config.py
@@ -8,11 +8,13 @@ class Config:
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
+    DATABASE_URL='postgresql://postgres:1998@localhost:5432/politico_db'
 
 class TestingConfig(Config):
     """Configurations for Testing."""
     TESTING = True
     DEBUG = True
+    DATABASE_URL='postgresql://postgres:1998@localhost:5432/test_politico_db'
 
 
 app_config = {  
