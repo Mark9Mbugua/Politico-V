de776c03d9776f855b1d9a5254403635c0422eb2
Mark Mbugua <mbuguamark@gmail.com>
2019-02-14 09:37:04 +0300

    [Starts #163932126] persist endpoints using Postgres

--- app/api/v2/models/office_models.py
+++ app/api/v2/models/office_models.py
@@ -1,13 +1,57 @@
-class PoliticalOffices:
+from app.db_config import init_db
+import psycopg2
+import itertools
 
+class PoliticalOffices():
     def __init__(self):
-        pass
+        self.db = init_db()
     
-    def create_office(self, office_type, name):
-        pass
+    #serializes data into json object format
+    def serializer(self, office):
+        office_fields = ('party_id', 'office_name', 'office_type')
+        result = dict()
+        for index, field in enumerate(office_fields):
+            result[field] = office[index]
+        return result
+    
+    def serializer_two(self, office_details):
+        office_id, office_name, office_type = office_details
+        result = dict(office_id = office_id, office_name = office_name, office_type = office_type)
+        return result
 
-    def get_offices(self):
-        pass
+    def create(self, office_name, office_type):
+        cur = self.db.cursor()
+        query = """INSERT INTO offices(office_name, office_type)
+                VALUES (%s,%s) RETURNING office_id"""
+        content = (office_name, office_type)
+        cur.execute(query, content)
+        office_id = cur.fetchone()
+        self.db.commit()
+        cur.close()
+        return self.serializer(tuple(itertools.chain(office_id, content)))
+    
+    def get_all_offices(self):
+        cur = self.db.cursor()
+        query = """SELECT office_id, office_name, office_type FROM offices"""
+        columns = ('office_id', 'office_name', 'office_type')
+        cur.execute(query)
+        office_details = cur.fetchall()
+        if office_details:
+            offices = []
+            for detail in office_details:
+                office_values = []
+                for value in detail:
+                    office_values.append(str(value))
+                offices.append(dict(zip(columns, office_values)))
+            return offices
+        return None
 
     def get_one_office(self, office_id):
-       pass
+        cur = self.db.cursor()
+        query = """SELECT * FROM offices WHERE office_id = {}""".format(office_id, )
+        cur.execute(query)
+        office = cur.fetchone()
+        cur.close()
+        if office:
+            return self.serializer_two(office)
+        return None
--- app/api/v2/views/party_views.py
+++ app/api/v2/views/party_views.py
@@ -1,25 +1,77 @@
 from flask import Flask, jsonify, request, make_response, Blueprint
-from app.api.v1.models.party_models import PoliticalParties
+from app.api.v2.models.party_models import PoliticalParties
+from app.api.v2.utils.validators import Validators
+from app.api.v2.utils.logo_validator import LogoUrlValidator
+from app.api.v2.utils.serializer import Serializer
 
 pv2 = Blueprint('ap4', __name__, url_prefix='/api/v2')
 
 
 @pv2.route('/parties', methods=['POST']) 
 def post_party():
-    return None
+    """route for creating a new political party"""
+    parties = PoliticalParties().get_all_parties()
+    try:
+        data = request.get_json()
+        party_name = data['party_name']
+        hqAddress = data['hqAddress']
+        logoUrl = data['logoUrl']        
+        party = PoliticalParties().create(party_name, hqAddress, logoUrl)
+        response = Validators().party_data_validator(party_name, hqAddress)
+        result = LogoUrlValidator().validate_logo_url(logoUrl)
+    except KeyError:
+        return Serializer.error_serializer('One or more keys is missing', 400), 400
     
+    if response == True:
+        if result == True:
+            return Serializer.json_serializer('Political party created successfully', party, 201), 201
+        return make_response(jsonify(result), 400)
+    
+    return make_response(jsonify(response), 400)
+    
+
 @pv2.route('/parties', methods=['GET']) 
 def get_parties():
-    return None
+    parties = PoliticalParties().get_all_parties()
+    if parties:
+        return Serializer.json_serializer('All political parties retrieved successfully', parties, 200), 200
+    
+    return Serializer.error_serializer('Political parties cannot be found', 404), 404
 
 @pv2.route('/parties/<int:party_id>', methods=['GET']) 
 def get_party(party_id):
-    return None
+    party = PoliticalParties().get_one_party(party_id)
+    
+    if party:
+       return Serializer.json_serializer('Political party retrieved successfully', party, 200), 200
+    
+    return Serializer.error_serializer('Political party cannot be found', 404), 404
+
 
 @pv2.route('/parties/<int:party_id>', methods=['PATCH']) 
 def update_party(party_id):
-    return None
+    try:
+        data = request.get_json()
+        party_name = data['party_name']
+        edit_party = PoliticalParties().edit_party(party_id, party_name)
+    except KeyError:
+        return Serializer.error_serializer('Party name key is missing', 400), 400
+
+    if edit_party:
+        if party_name == "" or party_name.isspace():
+            return Serializer.error_serializer('Party name is required', 400), 400
+        
+        if not all(x.isalpha() or x.isspace() for x in party_name):
+            return Serializer.error_serializer('Name should only have letters and spaces', 400), 400
+        
+        return Serializer.json_serializer('Political party updated successfully', edit_party, 200), 200
+    
+    return Serializer.error_serializer('Political party cannot be found', 404), 404
 
 @pv2.route('/parties/<int:party_id>', methods=['DELETE'])
 def delete_party(party_id):
-    return None
+    delete_party = PoliticalParties().delete_party(party_id)
+    if delete_party:
+        return Serializer.json_serializer('Political Party deleted successfully', None, 200), 200
+    
+    return Serializer.error_serializer('Political party cannot be found', 404), 404
