265c64ccc0e70fdddc4bb11b2d51dcc138eb4621
Mark Mbugua <mbuguamark@gmail.com>
2019-02-21 12:02:05 +0300

    testing db

--- app/db_config.py
+++ app/db_config.py
@@ -2,8 +2,10 @@ import psycopg2
 import os
 from passlib.hash import pbkdf2_sha256 as sha256
 
-db_url = 'postgresql://postgres:1998@localhost:5432/politico_db'
+# db_url = 'postgresql://postgres:1998@localhost:5432/politico_db'
+db_url = 'postgres://wxdaktrvhevvma:58c93ca91945a567e020f8ef8240d8668aa945ff255537f961d97059ce25d3bf@ec2-23-21-165-188.compute-1.amazonaws.com:5432/daulhq4hucu8ip'
 test_db_url = 'postgresql://postgres:1998@localhost:5432/test_politico_db'
+
 
 def connection(url):
     con = psycopg2.connect(url)
