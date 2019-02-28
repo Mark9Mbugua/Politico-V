e68855a0eb80168f8727b95cbc87c59115fd3d7c
Mark Mbugua <mbuguamark@gmail.com>
2019-02-28 22:37:46 +0300

    [Feature #164141124] heroku app/db debug

--- instance/config.py
+++ instance/config.py
@@ -26,7 +26,7 @@ class StagingConfig(Config):
 class ProductionConfig(Config):
     """Configurations for Production."""
     DATABASE_URL = os.getenv('PROD_DATABASE_URL')
-    DEBUG = True
+    DEBUG = False
     TESTING = False
 
 app_config = {  
